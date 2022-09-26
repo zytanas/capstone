0f7c176e54f6a49a9ed825f825f15b577c267756
hailzytanas <92881119+zytanas@users.noreply.github.com>
2022-09-26 16:18:29 +0800

    initial commit

---
+++ assets/dummy-data/navLinks.js
@@ -0,0 +1,35 @@
+const navLinks = [
+  {
+    path: "/dashboard",
+    icon: "ri-apps-2-line",
+    display: "Dashboard",
+  },
+  {
+    path: "/inventory",
+    icon: "ri-shopping-bag-line", 
+    display: "Inventory",
+  },
+  {
+    path: "/order",
+    icon: "ri-layout-grid-line",
+    display: "Orders",
+  },
+  {
+    path: "/return",
+    icon: "ri-arrow-go-back-line",
+    display: "Returns",
+  },
+  {
+    path: "/contact",
+    icon: "ri-phone-line",
+    display: "Contacts",
+  },
+  {
+    path: "/user",
+    icon: "ri-team-line",
+    display: "Users",
+  },
+
+];
+
+export default navLinks;
---
+++ assets/dummy-data/recommendCars.js
@@ -0,0 +1,32 @@
+import med1 from "../images/med-07.png";
+import med2 from "../images/med-08.png";
+import med3 from "../images/med-09.png";
+
+const recommendCars = [
+  {
+    id: "01",
+    medName: "Surgical Masks",
+    rentPrice: 32,
+    retweet: "132",
+    imgUrl: med1,
+    percentage: "74",
+  },
+  {
+    id: "02",
+    medName: "Weighing Scale",
+    rentPrice: 28,
+    retweet: "130",
+    imgUrl: med2,
+    percentage: "74",
+  },
+  {
+    id: "03",
+    medName: "Drain Bag",
+    rentPrice: 28,
+    retweet: "130",
+    imgUrl: med3,
+    percentage: "74",
+  },
+];
+
+export default recommendCars;
